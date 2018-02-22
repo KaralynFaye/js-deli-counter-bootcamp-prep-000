e5d0466bc5f2ce8fc8c3e6b31cd6af580cd758f5
Karalyn Ferrari <lifesmygift@gmail.com>
2018-02-22 16:14:13 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,32 @@
+var deli= [];
+
+function takeANumber(line, name) {
+  line.push(name);
+  return line.length;
+}
+
+function nowServing(line) {
+  if (line.length > 0) {
+    return line.pop();
+  }
+  
+  else {
+    return 'There is nobody waiting to be served!';
+  }
+}
+
+function currentLine(line) {
+  if (line.length > 0){
+    var list = 'The line is currently:'
+    for (var i = 0; i < line.length; i++) {
+      list = list + ` ${i+1}. ${line[i]},`
+    }
+    return list;
+  }
+  else {
+    return 'The line is currently empty.'
+  }
+}
+
+
+
